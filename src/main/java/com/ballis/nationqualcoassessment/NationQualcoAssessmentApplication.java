6106a84bcdcd9dffb17cd531f527c422dd9d8626
ballisel <ballislefteris@gmail.com>
2023-11-05 08:51:10 +0200

    initial commit

---
+++ src/main/java/com/ballis/nationqualcoassessment/NationQualcoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.ballis.nationqualcoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationQualcoAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationQualcoAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ballis/nationqualcoassessment/NationQualcoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballis.nationqualcoassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationQualcoAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
